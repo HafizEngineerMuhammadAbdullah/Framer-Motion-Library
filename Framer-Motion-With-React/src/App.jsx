fc285abf0ae1cde6308ae160f10ded5070e88a76
HafizEngineerMuhammadAbdullah <abdullahkhalid2k25@gmail.com>
2026-06-26 11:11:23 -0700

    Learning Framer Motion

--- Framer-Motion-With-React/src/App.jsx
+++ Framer-Motion-With-React/src/App.jsx
@@ -4,6 +4,20 @@ import { motion } from 'motion/react';
 const App = () => {
   return (
     <div>
+      <motion.h1
+        initial={{//similar to 'from'
+          x: -500
+        }}
+        animate={{//similar to 'to'
+          x: 1000,
+          rotate: 360
+        }}
+        transition={{
+          duration: 2,
+          repeat: Infinity,
+          //repeat: 1,
+        }}
+      >Subscribe me!</motion.h1>
       <motion.div
         className="box"
         animate={{
@@ -12,7 +26,7 @@ const App = () => {
           rotate: 360
         }}
         transition={{
-          duration:3,
+          duration: 3,
           delay: 2
         }}
       ></motion.div>
@@ -25,6 +39,36 @@ const App = () => {
         }}
       ></motion.div>
       <motion.h1>Welcome to React HomeLand!</motion.h1>
+      <motion.img
+        animate={{
+          x: 1000,
+          rotate: 360
+        }}
+        transition={{
+          duration: 2,
+          delay: 1,
+          repeat: Infinity,
+          ease: 'anticipate'
+        }}
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvgZmQHFlFZyGSHDaAqmKsvs1RwuVZcD2VhQ&s" alt="" />
+      <motion.div 
+       initial={{
+        x:0,
+        y:0
+       }}
+       animate={{
+         x:[0,800,800,0,0],
+         y:[0,0,300,300,0],
+         rotate: [0,360,0,-360,0]
+       }}
+
+       transition={{
+        duration: 2,
+        delay: 1,
+        repeat: 2,
+        ease: 'anticipate'
+       }}
+      className="box"></motion.div>
     </div>
   )
 }
